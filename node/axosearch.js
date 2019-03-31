f6016fdaf5a29727db965049e91c1d180f5d1848
Gregory Jarrige <gregory.jrrg@gmail.com>
2019-03-31 21:33:23 +0200

    add file diriterator

--- node/axosearch.js
+++ node/axosearch.js
@@ -4,61 +4,17 @@ const fs = require('fs');
 const path = require('path');
 const port = 8081;
 
+const filewalker = require('./diriterator.js').filewalker;
+const getExtension = require('./diriterator.js').getExtension;
+const getFileContents = require('./diriterator.js').getFileContents;
+
 var search_path = "/home/gregja/Documents/axoloti_works"; // personalize the path
-
-/**
- * Explores recursively a directory and returns all the filepaths and folderpaths in the callback.
- *
- * @see http://stackoverflow.com/a/5827895/4241030
- * @param {String} dir
- * @param {Function} done
- */
-function filewalker(dir, done) {
-    let results = [];
-
-    fs.readdir(dir, function(err, list) {
-        if (err) return done(err);
-
-        var pending = list.length;
-
-        if (!pending) return done(null, results);
-
-        list.forEach(function(file){
-            file = path.resolve(dir, file);
-
-            fs.stat(file, function(err, stat){
-                // If directory, execute a recursive call
-                if (stat && stat.isDirectory()) {
-                    // Add directory to array [comment if you need to remove the directories from the array]
-                    results.push(file);
-
-                    filewalker(file, function(err, res){
-                        results = results.concat(res);
-                        if (!--pending) done(null, results);
-                    });
-                } else {
-                    results.push(file);
-                    if (!--pending) done(null, results);
-                }
-            });
-        });
-    });
-}
-
-function getExtension(path) {
-    return path.split('.').pop();
-}
-
-function getFileContents(path) {
-    return fs.readFileSync(path).toString();
-}
 
 function checkStringOccurs(haystack, needle) {
     return haystack.indexOf(needle) != -1 ? true : false;
 }
 
 function searchFiles(res, params, callback) {
-    console.log(params)
     var searchdata = params.searchdata;
     if (params.searchtype == 'type') {
         searchdata = 'type="'+searchdata ;
